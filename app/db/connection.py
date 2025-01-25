c9ec5807e7dcba816ba56d535556593f6994cada
Natanael Monteiro Lima <dr.natanael.lima@gmail.com>
2025-01-25 17:18:27 -0300

    Ajustar conexao

--- app/db/connection.py
+++ app/db/connection.py
@@ -1,9 +1,10 @@
-import os
+# import os
 from decouple import config
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DB_URL = config(os.environ.get("DB_URL"))
+DB_URL = config("DB_URL")
+# DB_URL = config(os.environ.get("DB_URL"))
 
 engine = create_engine(DB_URL, pool_pre_ping=True)
 DBSession = sessionmaker(bind=engine)
